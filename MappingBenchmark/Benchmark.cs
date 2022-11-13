80bc73e87fbda8ae4450c57b5153ed33a477cf25
Daniel Cazzulino <daniel@cazzulino.com>
2022-11-13 20:03:11 -0300

    Clean warnings, update switch expression

--- MappingBenchmark/Benchmark.cs
+++ MappingBenchmark/Benchmark.cs
@@ -12,25 +12,28 @@ using Xunit;
 namespace DynamicBenchmark;
 
 [MemoryDiagnoser]
-//[Orderer(BenchmarkDotNet.Order.SummaryOrderPolicy.FastestToSlowest)]
 [SimpleJob(RuntimeMoniker.Net60)]
 [SimpleJob(RuntimeMoniker.Net70)]
 [SimpleJob(RuntimeMoniker.Net472, baseline: true)]
 //[RPlotExporter]
 public class DynamicPerformance
 {
-    private object data;
-    private MemoryStream jsonStream;
-    private MemoryStream mPackStream;
-    private IMapper mapper;
+    object data;
+    MemoryStream jsonStream;
+    MemoryStream mPackStream;
+    IMapper mapper;
 
     //[Params(10, 100, 1000)]
     public int Lines = 100;
 
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
     public DynamicPerformance() => Setup();
-    
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+
     [GlobalSetup]
+#pragma warning disable xUnit1013 // Public method should be marked as test
     public void Setup()
+#pragma warning restore xUnit1013 // Public method should be marked as test
     {
         var lines = new List<dynamic>();
 
@@ -54,7 +57,7 @@ public class DynamicPerformance
 
         mPackStream = new MemoryStream();
         MessagePackSerializer.Serialize(typeof(Buffer), mPackStream, JsonSerializer.Deserialize<Buffer>(jsonStream));
-
+        
         var configuration = new MapperConfiguration(cfg =>
         {
             cfg.CreateMap(((object)data).GetType(), typeof(Buffer));
@@ -119,14 +122,12 @@ static partial class Dynamically
             global::System.Console.WriteLine("Is provider!");
         }
 
-        switch (typeof(T))
+        return typeof(T) switch
         {
-            case Type t when t == typeof(Buffer): return (T)Buffer.Create(data);
-            case Type t when t == typeof(Line): return (T)Line.Create(data);
-            case Type t when t == typeof(Point): return (T)Point.Create(data);
-            default: throw new NotSupportedException();
-        }
-
-        throw new NotSupportedException();
+            Type t when t == typeof(Buffer) => (T)Buffer.Create(data),
+            Type t when t == typeof(Line) => (T)Line.Create(data),
+            Type t when t == typeof(Point) => (T)Point.Create(data),
+            _ => throw new NotSupportedException(),
+        };
     }
 }
